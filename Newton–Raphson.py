97d612fcc9165dbdb57bea8e91c7712e69f6e7a8
qianxliuC <155934142+qianxliuC@users.noreply.github.com>
2024-01-19 15:52:40 +0800

    Update Newton–Raphson.py
    forget break, CS

--- Newton–Raphson.py
+++ Newton–Raphson.py
@@ -2,10 +2,10 @@ import math
 
 target = 0
 
-def f(x : float) -> float:
+def f(x: float) -> float:
     return math.pow(9,x) + math.pow(25, x) - math.pow(45, x);
 
-def df(x : float) -> float:
+def df(x: float) -> float:
     return math.pow(9,x)*math.log(9) + math.pow(25, x)*math.log(25) - math.pow(45, x)*math.log(45);
 
 def newton(inital_value: float, threshold: float = 1e-65535, epsilon: float = 1e-255) -> tuple[bool, float, float, float]:
@@ -20,6 +20,7 @@ def newton(inital_value: float, threshold: float = 1e-65535, epsilon: float = 1e
         dy = df(x)
         if (abs(dy) < epsilon):
             unlimited = True
+            break
         print(f"Calculated values are x={x}, y={y}, dy={dy}")
     
     return unlimited, x, y, dy
